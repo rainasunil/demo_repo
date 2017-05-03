e6d3ef10cea09e37b8c03303f325bd5b15b51432
rainasunil <rainasunil.k@gmail.com>
2017-05-03 09:37:32 +0530

    Add files via upload

---
+++ jenkins/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("say hello to jenkins");
+            Console.ReadKey();
+        }
+    }
+}
