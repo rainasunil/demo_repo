e264291e87b6d634cdf23bbf03d20b88739f7930
sunil kumar raina <sunilraina.k@in.abb.com>
2017-05-03 10:29:51 +0530

    added

---
+++ UnitTestProject/UnitTestProject/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int sum =0;
+            sum = sum + 10;
+            Console.WriteLine("sum of two nos = {0}", sum);
+        }
+    }
+}
